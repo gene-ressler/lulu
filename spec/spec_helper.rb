c1bbc4d52f667d05f3bc4fb70d1f5dc70940a534
Gene Ressler <gene.ressler@gmail.com>
2014-05-11 20:30:05 -0400

    Clean marker list test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,6 @@ TEST_SIZE = 10000
 def new_marker_list(size = TEST_SIZE)
   list = Lulu::MarkerList.new
   srand(42)
-  TEST_SIZE.times{ |n| list.add(Random.rand(1000), Random.rand(1000), Random.rand(100)) }
+  size.times{ list.add(Random.rand(1000), Random.rand(1000), Random.rand(100)) }
   list
 end
